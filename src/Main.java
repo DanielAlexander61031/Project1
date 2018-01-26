da2a1b629d04c51031d7410a13362d9973699067
61031 <dalexander.61031@farnborough.ac.uk>
2018-01-26 13:38:44 +0000

    The third update of the project

--- src/Main.java
+++ src/Main.java
@@ -42,3 +42,63 @@ public class Main extends Application {
         root.getChildren().add(list);
 
 
+        stage.setOnCloseRequest((WindowEvent we) -> exitPrompt(we));
+    }
+    // This is the start-up sequence of coding that would produce the second scene
+    public static void openNewStage(Pane parent) {
+        StageTwo newStage = new StageTwo(parent);
+    }
+
+    public static void exitPrompt(WindowEvent we) {
+// This is the exit confirmation code that would exit the program upon pressing the 'yes' button
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Quit 'Quizmaker'?");
+        alert.setHeaderText("Are you sure you want to exit?");
+
+        Optional result = alert.showAndWait();
+        if (result.get() == ButtonType.OK) {
+            System.exit(0);
+        } else {
+            we.consume();
+        }
+
+    }
+    // This was the working class that controls the second scene
+    public static class StageTwo {
+
+        static Pane parent;
+
+        public StageTwo(Pane theParent) {
+
+            Stage stage = new Stage();
+            parent = theParent;
+            parent.setDisable(true);
+            start(stage);
+
+        }
+
+        public void start(Stage stage) {
+
+            Pane root = new Pane();
+            Scene scene = new Scene(root, 1024, 768);
+            stage.setTitle("Choose your Quiz and play.");
+            stage.setScene(scene);
+            stage.setOnCloseRequest((WindowEvent we) -> closeStage(stage));
+            stage.show();
+// This button was made to start the quiz
+            Button my2Button = new Button("Start.");
+            my2Button.setLayoutX(800);
+            my2Button.setLayoutY(710);
+            my2Button.setPrefSize(200, 35);
+            my2Button.setOnAction((ActionEvent ae) -> openNewStage(root));
+            root.getChildren().add(my2Button);
+// This is the coding that controlled the combobox, this held the different quiz types and was mapped to the appropriate txt files
+            ObservableList Quizzes = FXCollections.observableArrayList("Quiz 1", "Quiz 2", "Quiz 3", "Quiz 4", "Quiz 5");
+            ComboBox comboBox = new ComboBox(Quizzes);
+            comboBox.setLayoutX(100);
+            comboBox.setLayoutY(100);
+            comboBox.getSelectionModel().select(0);
+
+            root.getChildren().add(comboBox);
+        }
+
